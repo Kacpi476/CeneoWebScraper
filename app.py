35da39abe18d6d020e1ff5908f52f1662636eaf5
Kacpi476 <kapiszixd2@gmail.com>
2024-03-13 01:20:24 +0100

    4th commit - code correction

--- app.py
+++ app.py
@@ -58,15 +58,40 @@ def scrape_data(product_id):
             yes = comment.find('span', id=f"votes-yes-{int(comment_id)}")
             no = comment.find('span', id=f"votes-no-{int(comment_id)}")
 
-            opinie_data.append({
-                "author": author.text.strip() if author else "Brak autora",
-                "recomendation": author_recomendation.text.strip() if author_recomendation else "Brak polecenia",
-                "user_score": user_score.text if user_score else "Brak oceny użytkownika",
-                "purchase_confirmation": "Potwierdzenie zakupu" if purchase_confirmation else "Brak potwierdzenia",
-                "yes_count": yes.text if yes else "Brak licznika pozytywnych opinii",
-                "no_count": no.text if no else "Brak licznika negatywnych opinii",
-                "text": user_text.text if user_text else "Brak tekstu opinii",
-            })
+            opinia_data = {
+                "id": comment_id,
+                "author": author.text.strip() if author else "",
+                "recomendation": author_recomendation.text.strip() if author_recomendation else "",
+                "user_score": user_score.text if user_score else "",
+                "purchase_confirmation": "Potwierdzenie zakupu" if purchase_confirmation else "",
+                "yes_count": yes.text if yes else "",
+                "no_count": no.text if no else "",
+                "text": user_text.text if user_text else "",
+            }
+            elements_with_timeline = comment.find_all(attrs={"datetime": True})
+            opinia_data["date_posted"] = elements_with_timeline[0]["datetime"] if elements_with_timeline else ""
+            opinia_data["date_purchased"] = elements_with_timeline[1]["datetime"] if len(elements_with_timeline) > 1 else ""
+
+            licznik_pros_cons = 0
+            pros_cons = comment.find_all("div", class_="review-feature__col")
+            if pros_cons:
+                zalety = []
+                wady = []
+                for element in pros_cons:
+                    items = element.find_all("div", class_="review-feature__item")
+                    for item in items:
+                        if licznik_pros_cons == 0:
+                            zalety.append(item.text)
+                        if licznik_pros_cons == 1:
+                            wady.append(item.text)
+                    licznik_pros_cons += 1
+                opinia_data["zalety"] = zalety
+                opinia_data["wady"] = wady
+            else:
+                opinia_data["zalety"] = ""
+                opinia_data["wady"] = ""
+
+            opinie_data.append(opinia_data)
 
             counter += 1
             count += 1
@@ -80,16 +105,19 @@ def home():
 
 @app.route('/extract')
 def index():
-    return render_template('extract.html')
+    return render_template('extract.html', message="Produkt został dodany")
 
 @app.route("/add_product", methods=['POST'])
 def add_product():
     product_id = request.form['product_id']
-    if is_valid_product_id(product_id) and check_product_exists(product_id):
-        if product_id not in products:
-            products.append(product_id)
-            scrape_data(product_id)
-    return redirect(url_for('extract'))
+    if not product_id or not is_valid_product_id(product_id) or not check_product_exists(product_id):
+        return render_template('extract.html', message="Nieprawidłowy kod produktu.")
+    elif product_id in products:
+        return render_template('extract.html', message="Ten produkt został już dodany.")
+    else:
+        products.append(product_id)
+        scrape_data(product_id)
+        return redirect(url_for('extract'))
 
 
 @app.route("/data")
